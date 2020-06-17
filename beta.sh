73972de19d86884e6d09a598903b3b09d0fb3891
NorthernMan54 <buyminivan-northern@yahoo.ca>
2020-06-16 21:11:47 -0400

    Fixes for multi

---
+++ beta.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+if npm audit; then
+  npm run-script document
+  rm *orig* *toc\.*
+  git add .
+  npm version patch -m "$1" --force
+  npm publish --tag beta
+  git commit -m "$1"
+  git push origin multiThermo --tags
+else
+  echo "Not publishing due to security vulnerabilites"
+fi
