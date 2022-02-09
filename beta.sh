cac84d7fd785a57d528844e388a5d93ca863c6c2
Northern Man <northern.man1@gmail.com>
2022-02-09 18:52:03 -0500

    Potential fix for not responding

--- beta.sh
+++ beta.sh
@@ -4,7 +4,7 @@ if npm audit; then
   npm run-script document
   rm *orig* *toc\.*
   git add .
-  npm version patch -m "$1" --force
+  npm version prerelease --preid beta -m "$1" --force
   npm publish --tag beta
   git commit -m "$1"
   git push origin beta --tags
